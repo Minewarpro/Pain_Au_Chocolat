d3e91c480e5f5b887211b325100a7c9938bc7dfc
kancabricot <timothee.fontan@gmail.com>
2022-05-14 20:52:45 +0200

    nice l'amitier arrive dans vos maison

---
+++ Alpha wtf/src/ZoneAmis.js
@@ -0,0 +1,43 @@
+class ZoneAmis {
+
+    constructor(scene, player1, player2) {
+        this.scene = scene
+        this.player1 = player1
+        this.player2 = player2
+
+        this.friend = false;
+        this.compteur = 0;
+
+        const map = this.scene.make.tilemap({key: 'map'});
+
+        this.zoneAmis = this.scene.physics.add.group({
+            allowGravity: false,
+            immovable: true
+        });
+        map.getObjectLayer('ZoneAmis').objects.forEach((Box) => {
+            this.collideSprite = this.scene.physics.add.sprite(Box.x + (Box.width * 0.5), Box.y + (Box.height * 0.5)).setSize(Box.width, Box.height);
+            this.zoneAmis.add(this.collideSprite)
+        });
+        this.scene.physics.add.overlap(this.player1.player, this.zoneAmis);
+        this.scene.physics.add.overlap(this.player2.player, this.zoneAmis);
+
+
+    }
+
+    FunctionUpdate(){
+        if(this.scene.physics.overlap(this.player1.player, this.zoneAmis)===true && this.scene.physics.overlap(this.player2.player, this.zoneAmis)===true ){
+            console.log("yesman");
+            this.friend = true;
+        }
+        if(this.friend === true){
+            this.compteur += 1;
+            if(this.compteur === 500){
+                console.log("amis")
+            }
+        }else{
+            this.compteur = 0;
+        }
+
+    }
+
+}
